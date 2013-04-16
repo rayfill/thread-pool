4aacf26a664a0d9a25d6148cb01bbaf59ae844e3
Jiro OKAZAKI <rayfill@gmail.com>
2013-04-16 11:02:14 +0900

    fix synchronization system.

--- src/thread-pool.lisp
+++ src/thread-pool.lisp
@@ -5,15 +5,18 @@
   (:import-from :queues :put :offer :take :poll :peek)
   (:import-from :blocking-queue :blocking-queue)
   (:import-from :workers :create-workers :wait-workers
-		:kill-workers :workers-count)
+		:kill-workers :workers-count :workers-start-p)
   (:import-from :local-time :now :sec-of :nsec-of)
   (:import-from :sb-thread :thread :join-thread
 		:make-semaphore :try-semaphore :semaphore-count)
-  (:import-from :execute-policy :abort-queuing :caller-runs
+  (:import-from :execute-policy :abstract-policy
+		:abort-queuing :caller-runs
 		:discard-oldest :discard
 		:queue-flood)
   (:export :thread-pool :start
-	   :wait-shtudown-complete :shutdown :execute))
+	   :wait-shtudown-complete :shutdown :execute :with-thread-pool
+	   :abort-queuing :caller-runs :discard-oldest :discard
+	   :*default-thread-pool*))
 
 (in-package :thread-pool)
 
@@ -26,12 +29,16 @@
 	 (poison-pill :initform (gensym "POISON"))))
 
 (defmethod initialize-instance :after ((instance thread-pool)
-				       &key (worker-count 8)
+				       &key
+				       (autostart-p nil)
+				       (worker-count 8)
 				       (pool-size nil is-pool-size-set-p)
 				       (interval 0)
 				       (times 10)
-				       (exec-policy 'caller-run))
-  (declare (type integer pool-size interval times))
+				       (exec-policy 'caller-runs))
+  (declare (type integer worker-count interval times)
+	   (type (or integer null) pool-size)
+	   (type (or abstract-policy symbol) exec-policy))
   (with-slots (queue workers poison-pill check-interval check-times policy)
       instance
     (let ((pool-size (if is-pool-size-set-p pool-size (* worker-count 10))))
@@ -45,10 +52,20 @@
 	    check-interval interval
 	    check-times times
 	    poison-pill (gensym)
-	    policy exec-policy))))
+	    policy (if (typep exec-policy 'execute-policy::abstract-policy)
+		       exec-policy (make-instance exec-policy)))))
+  (when autostart-p
+    (start instance)))
+
+(defun is-shutdown (instance)
+  (with-slots (is-shutdown)
+      instance
+    (= (semaphore-count is-shutdown) 0)))
 
 (defgeneric start (instance))
 (defmethod start ((instance thread-pool))
+  (when (is-shutdown instance)
+    (error 'simple-error :format-control "already shutdown started."))
   (with-slots (workers)
       instance
     (workers:start-workers workers)))
@@ -57,7 +74,9 @@
 (defmethod execute ((instance thread-pool) work)
   (unless (functionp work)
     (error 'simple-error :format-control "invalid argument: work, not a function."))
-  (with-slots (check-interval check-times policy queue)
+  (when (is-shutdown instance)
+    (error 'simple-error :format-control "thread-pool is already shutdown progress."))
+  (with-slots (check-interval check-times policy queue workers)
       instance
     (loop
        for rest-times from check-times downto 1
@@ -68,17 +87,23 @@
        do (sleep check-interval)
        end)))
 
-(defgeneric shutdown (instance &key wait-p))
-(defmethod shutdown ((instance thread-pool) &key (wait-p nil))
+(defgeneric shutdown (instance &optional wait-p))
+(defmethod shutdown ((instance thread-pool) &optional wait-p)
   (with-slots (queue workers is-shutdown poison-pill)
       instance
     (let ((number-of-threads (workers-count workers)))
       (unless (try-semaphore is-shutdown)
 	(error 'simple-error :format-control "shutdown is already started."))
+      (unless (workers-start-p workers)
+	(loop
+	   for (nil suc) = (multiple-value-list (queues:poll queue))
+	   while suc
+	   finally (workers:start-workers workers)))
       (loop for i below number-of-threads 
 	 do (queues:put queue poison-pill))
       (when wait-p
-	  (wait-workers workers)))))
+	  (wait-workers workers)
+	  (values)))))
 
 (defun time-to-nsecs (time)
   (let ((secs (sec-of time))
@@ -125,4 +150,13 @@
 			   (sleep check-interval)))))
 	 when (<= limit-time 0)
 	 return nil))))
-	  
+
+(defmacro with-thread-pool ((sym &rest key-values) &body body)
+  `(let ((,sym (make-instance 'thread-pool ,@key-values)))
+     (unwind-protect
+	  (progn
+	    (start ,sym)
+	    ,@body)
+       (shutdown ,sym t))))
+
+(defvar *default-thread-pool* (make-instance 'thread-pool))
--- t/thread-pool.lisp
+++ t/thread-pool.lisp
@@ -11,14 +11,20 @@
 
 (plan nil)
 
-(let ((tp (make-instance 'thread-pool
-			 :exec-policy (make-instance 'abort-queuing)
-			 :pool-size 10 :worker-count 10)))
-  (let ((count (loop for i from 0
-		  while (execute tp (lambda ()
-				      (sleep 5)
-				      1))
-		  finally (return i))))
-    (ok count 20)))
+(let ((sum (make-array 1 :element-type 'sb-ext:word :initial-element 0))
+      (out *standard-output*))
+  (ok (>= (with-thread-pool (tp :exec-policy (make-instance 'abort-queuing)
+				:pool-size 10 :worker-count 10)
+	    (let ((count (loop for i from 0
+			    while (let ((i i))
+				    (execute tp (lambda ()
+						  (sleep 3)
+						  (format out "call increment.~A~%" i)
+						  (sb-ext:atomic-incf (aref sum 0)))))
+			    do (format t "queuing...~A~%" i)
+			    finally (return i))))
+	      count)) 10)) ;; (ok (>= count 10))
+  (format t "sum: ~A~%" (aref sum 0))
+  (ok (>= (aref sum 0) 10) (format nil "actualy sum is ~A~%" (aref sum 0))))
 
 (finalize)
